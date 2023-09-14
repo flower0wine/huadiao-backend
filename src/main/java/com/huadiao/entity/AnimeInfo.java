9854209922267b6e0f90ef266b8123ea5f30a918
flowerwine <2914518090@qq.com>
2023-09-14 16:00:54 +0800

    花凋后端修改时间: 2023-09-14

---
+++ src/main/java/com/huadiao/entity/AnimeInfo.java
@@ -0,0 +1,23 @@
+package com.huadiao.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @author flowerwine
+ * @date 2023 年 09 月 12 日
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class AnimeInfo {
+    private String title;
+    private String cover;
+    private Integer animeId;
+    private Date addTime;
+}
