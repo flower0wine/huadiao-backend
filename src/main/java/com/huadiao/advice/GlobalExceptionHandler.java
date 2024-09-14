6c0e8840b3490f50a35720c81d729a61f50ed337
flowerwine <2914518090@qq.com>
2024-09-14 23:22:43 +0800

    chore: 全局异常捕获器统一返回 Result JSON 格式

--- src/main/java/com/huadiao/advice/GlobalExceptionHandler.java
+++ src/main/java/com/huadiao/advice/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.huadiao.advice;
 
+import cn.hutool.json.JSONUtil;
+import com.huadiao.entity.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -12,9 +15,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public void handleException(HttpServletRequest request, HttpServletResponse response, Exception ex) {
         response.setStatus(500);
+        response.setContentType("application/json;charset=UTF-8");
         ex.printStackTrace();
+        Result<?> result = Result.serverError(ex.getMessage());
         try {
-            response.getWriter().write(ex.getMessage());
+            response.getWriter().write(JSONUtil.toJsonStr(result));
         } catch (IOException e) {
             e.printStackTrace();
         }
